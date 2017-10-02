ef52034a3cbec81d4199b4e681a9319b9536e3ee
Kerry <kerry.kim.russo@gmail.com>
2017-10-01 22:09:59 -0400

    add initial routes; add redux with category placeholder data; render home page container

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,38 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducers from '../reducers';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import HomePageContainer from '../containers/home.page.container';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      </div>
-    );
-  }
-}
+const store = createStore(reducers, {
+  categories: [
+    {
+      name: 'react',
+      path: 'react'
+    },
+    {
+      name: 'redux',
+      path: 'redux'
+    },
+    {
+      name: 'udacity',
+      path: 'udacity'
+    }
+  ]
+});
+
+export const App = (props) => (
+  <Provider store={store}>
+    <Router>
+      <Route exact path="/" render={() => <HomePageContainer/>}/>
+      {/* <Route exact path="/category/:id" render={}/>
+      <Route exact path="/category/post/:id" render={}/>
+      <Route path="/post" render={}/>
+      <Route path="/post/new" render={}/>
+      <Route path="/post/:id/edit" render={}/> */}
+    </Router>
+  </Provider>
+);
 
 export default App;
