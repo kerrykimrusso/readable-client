a85bab07b2760c8e170bdeb36098497a86640fc3
Kerry <kerry.kim.russo@gmail.com>
2017-10-08 17:56:39 -0400

    add mock comment data

--- src/components/App.js
+++ src/components/App.js
@@ -41,7 +41,29 @@ const store = createStore(reducers, {
         voteScore: -5,
         deleted: false
       }
-    ]
+    ],
+    comments: [
+      {
+        id: '894tuq4ut84ut8v4t8wun89g',
+        parentId: "8xf0y6ziyjabvozdd253nd",
+        timestamp: 1468166872634,
+        body: 'Hi there! I am a COMMENT.',
+        author: 'thingtwo',
+        voteScore: 6,
+        deleted: false,
+        parentDeleted: false
+      },
+      {
+        id: '8tu4bsun805n8un48ve89',
+        parentId: "8xf0y6ziyjabvozdd253nd",
+        timestamp: 1469479767190,
+        body: 'Comments. Are. Cool.',
+        author: 'thingone',
+        voteScore: -5,
+        deleted: false,
+        parentDeleted: false
+      }
+    ],
   },
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
